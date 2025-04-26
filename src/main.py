5ac9f38d90a8eced83635a01c613b5a0b1df6478
serpenteno <fiacekk@gmail.com>
2025-04-26 21:19:12 +0200

    Add anonymization (w/ PLTE chunk check)

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import png_parser
 import os
 import fourier
+from png_anonymizator import anonymize_png
 
 if __name__ == "__main__":
-    file_path = os.path.join("assets", "game.png")
+    file_path = os.path.join("../assets", "game.png")
 
     chunks = png_parser.read_chunks(file_path)
 
@@ -14,3 +15,12 @@ if __name__ == "__main__":
 
     fourier.display_fourier_spectrum(file_path)
     fourier.test_fourier_transformation(file_path)
+
+    anonymized_file_path = os.path.join("../assets", "game_anonymized.png")
+    anonymize_png(file_path, anonymized_file_path)
+    chunks_anonymized = png_parser.read_chunks(anonymized_file_path)
+
+    for chunk in chunks_anonymized:
+        print(chunk)
+
+    png_parser.extract_metadata(chunks_anonymized)
